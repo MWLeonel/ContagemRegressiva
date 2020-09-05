ce9a473cd9e5a281c6456421d2df2b587b382a5f
MWLeonel <wmleonel6@gmail.com>
2020-09-05 11:04:18 -0300

    Calibre 38

---
+++ ContagemRegressiva/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("FOGO!");
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+        }
+    }
+}
